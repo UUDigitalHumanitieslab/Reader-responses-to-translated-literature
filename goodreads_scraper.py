101df09372bd41a8eed1a349b94c7cb7864570bc
R. Loeber <r.g.v.loeber@uu.nl>
2020-03-11 16:39:26 +0100

    scrapes all languages from selectlist, scrapes filtering on stars

--- goodreads_scraper.py
+++ goodreads_scraper.py
@@ -3,77 +3,74 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 from datetime import datetime
 import csv
 
-# For each review, store at least the following (not a complete list):
+###########################################################################################################
+# This script scrapes all the languages in the Goodreads selectbox, and will scrape the 5 - 1 star ratings. 
+# Data are stored in a csv file. Dat are appended if the review id is not yet in the csv
+# for each review an xml file is made and stored in the folder as specified in 'scraped_folder' variable.
+###########################################################################################################
 
+# For each review, store at least the following (not a complete list):
 #     edition title
 #     edition language
 #     review text
 #     review language
 #     review author
-#     review author sex (gender) HOW???
+#     review author sex (gender) HOW? not implemented yet
 #     review rating
 
 # as many other metadata as you can extract from GoodReads?
-# <a class="actionLinkLite loadingLink" rel="nofollow" data-keep-on-success="true" data-remote="true" href="#" onclick="new Ajax.Request('/book/reviews/40718205-the-dinner?rating=5&amp;amp;text_only=true', {asynchronous:true, evalScripts:true, method:'get', parameters:'authenticity_token=' + encodeURIComponent('wk0apYk4YJDnGPqsc23DrY71W0wike8TXvbXLp3UGfVi8iMF5vvGUzwOw9bf2VGzv5DELxlhzifHVA5HzNQH1g==')}); return false;" style="display: inline;">5 stars <span class="greyText">(12062)</span></a>
-# /book/reviews/40718205-the-dinner?rating=5&amp;amp;text_only=true
-
-# <select name="language_code" id="language_code"><option value="">All Languages</option><option value="af">Afrikaans ‎(1)</option>
-# <option value="az">Azərbaycan dili ‎(1)</option>
-# <option value="id">Bahasa Indonesia ‎(16)</option>
-# <option value="ca">Català ‎(1)</option>
-# <option value="da">Dansk ‎(2)</option>
-# <option value="de">Deutsch ‎(20)</option>
-# <option value="et">Eesti ‎(7)</option>
-# <option value="en">English ‎(14647)</option>
-# <option value="es">Español ‎(134)</option>
-# <option value="fr">Français ‎(22)</option>
-# <option value="it">Italiano ‎(85)</option>
-# <option value="lv">Latviešu valoda ‎(17)</option>
-# <option value="lt">Lietuvių kalba ‎(11)</option>
-# <option value="hu">Magyar ‎(3)</option>
-# <option selected="selected" value="nl">Nederlands ‎(370)</option>
-# <option value="no">Norsk ‎(1)</option>
-# <option value="pl">Polski ‎(4)</option>
-# <option value="pt">Português ‎(33)</option>
-# <option value="ru">Pусский язык ‎(21)</option>
-# <option value="ro">Română ‎(12)</option>
-# <option value="sk">Slovenčina ‎(1)</option>
-# <option value="sl">Slovenščina ‎(3)</option>
-# <option value="fi">Suomi ‎(17)</option>
-# <option value="sv">Svenska ‎(4)</option>
-# <option value="vi">Tiếng Việt ‎(1)</option>
-# <option value="tr">Türkçe ‎(8)</option>
-# <option value="hr">hrvatski ‎(2)</option>
-# <option value="is">Íslenska ‎(3)</option>
-# <option value="cs">česky, čeština ‎(3)</option>
-# <option value="el">Ελληνικά ‎(24)</option>
-# <option value="bg">български език ‎(55)</option>
-# <option value="mk">македонски јазик ‎(1)</option>
-# <option value="uk">українська ‎(2)</option>
-# <option value="he">עברית ‎(5)</option>
-# <option value="ar">العربية ‎(85)</option>
-# <option value="fa">فارسی ‎(2)</option>
-# <option value="th">ไทย ‎(1)</option>
-# <option value="ka">ქართული ‎(4)</option></select>
 
 
-# editions
-# https://www.goodreads.com/work/editions/6463092-het-diner
 
-#de reviews lijken niet te veranderen bij een andere editie
+def make_xml_file(scraped_folder, filename, title, review_date, review_language, review_id, author, rating, text):
 
-edition_url='https://www.goodreads.com/book/show/40718205-the-dinner'
-review_language='it'
+    xml="<scrape><title>" + title +"</title>"
+    xml+="<review_date>" + review_date +"</review_date>"
+    xml+="<review_language>" + review_language +"</review_language>"
+    xml+="<review_id>" + review_id + "</review_id>"
+    xml+="<author>" + author +  "</author>"
+    xml+="<rating>" + rating + "</rating>"
+    xml+="<text>" + text + "</text>"
+    xml+="</scrape>"
+
+    try: 
+        f = open(scraped_folder+"/" + review_id + "_" + filename +"_" + review_language +".xml",  "w", encoding="utf-8")
+        f.write(xml)
+        f.close()
+    except:
+        print (filename)
+        print ('kon geen bestand opslaan')
+
+
+
+def make_csv_file(title, review_date, review_language, review_id, author, rating, text):
+    
+    with open(r'scrapes_goodreads.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['review_id', 'title', 'review_date', 'review_language', 'author', 'rating', 'text']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({'review_id': review_id,'title':title, 'review_date':review_date, \
+        'review_language': review_language, 'author': author, 'rating':rating, 'text':text})
+
+
+def read_csv_file():
+
+    with open('scrapes_goodreads.csv', newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        return data
+
+
 
 def scrape_page(review_language):
 
     title='The dinner'
     filename='dinner'
-    edition_language='eng' # dit lijkt geen variabele in de reviews
+    edition_language='eng' # No existing variable in Goodreads as reviews are not attached to specific edition???
 
     review_id=''
     author=''
@@ -81,17 +78,26 @@ def scrape_page(review_language):
     rating=''
     scraped_folder='scraped_dinner'
     counter=0
-    reviews=driver.find_elements_by_class_name('friendReviews ')
+    reviews=driver.find_elements_by_class_name('friendReviews')
+
+    # read the csv with reviews that are already done
+    try:
+        existing_reviews=read_csv_file()
+    except:
+        existing_reviews=[]
     
+   
     for review in reviews:
         content=review.find_elements_by_css_selector("[id^='freeText']")
         review_id=review.find_element_by_class_name('review').get_attribute("id")
+        split= review_id.split("_")
+        review_id=split[1]
+
         review_date=review.find_element_by_class_name('reviewDate').text
-        review_date=review_date.strip(",") #remove ,
+        review_date=review_date.strip(",")
         
         try:
             author=review.find_element_by_css_selector("[class='user']").text
-            # author=author.encode("utf-8")
         except:
             author='unknown'
             print('no author found')
@@ -106,8 +112,7 @@ def scrape_page(review_language):
         print(review_date)
         print(author.encode("utf-8"))
         print(rating)
-        print('---------------------------------------------------------------------------')
-
+       
         if len(content) > 1:
             #there is a hidden part that forms the complete text. Is the second element
             text=content[1].get_attribute('textContent')
@@ -117,44 +122,100 @@ def scrape_page(review_language):
 
         #rating stars:
         # did not like is= 1, it was ok =2 , liked it =3, really liked it =4, it was amazing =5
-        
-        xml="<scrape><title>" + title +"</title>"
-        xml+="<review_date>" + review_date +"</review_date>"
-        xml+="<review_language>" + review_language +"</review_language>"
-        xml+="<review_id>" + review_id + "</review_id>"
-        xml+="<author>" + author +  "</author>"
-        xml+="<rating>" + rating + "</rating>"
-        xml+="<text>" + text + "</text>"
-        xml+="</scrape>"
+        review_exists=False
 
-        try: 
-            f = open(scraped_folder+"/" + review_id + "_" + filename +"_" + review_language +".xml",  "w", encoding="utf-8")
-            f.write(xml)
-            f.close()
-        except:
-            print (filename)
-            print ('kon geen bestand opslaan')
+        for existing_review in existing_reviews:
+            if review_id == existing_review[0]:
+                review_exists=True
+                print('Review already in csv, id = ' + review_id)
+                break
+
+        if review_exists == False:
+            make_csv_file(title, review_date, review_language, review_id, author, rating, text)
+            make_xml_file(scraped_folder, filename, title, review_date, review_language, review_id, author, rating, text)
+            print('Created new line in csv, created xml file')
 
         counter+=1
 
+        print('-------------------------------')
 
 
+def scrape_loop(driver, review_language, more_position):
 
-driver = webdriver.Firefox(executable_path=r'geckodriver\geckodriver.exe')
-driver.get(edition_url)
-
-for x in range(0, 10):
-    time.sleep(3)
+    time.sleep(2)
     ddelement= Select(driver.find_element_by_id('language_code'))
     ddelement.select_by_value(review_language)
 
-    time.sleep(4)
 
-    scrape_page(review_language)
+    if more_position != '': 
+        time.sleep(2)
 
-    try:
-        next_button=driver.find_element_by_class_name('next_page')
-        next_button.click()
-    except:
-        print('no next button')
-        break
+        filters = driver.find_element_by_css_selector("a[id^='span_class_gr-hyperlink_more_filters_']")
+        hover = ActionChains(driver).move_to_element(filters)
+        hover.perform()
+
+        tooltip=driver.find_element_by_class_name("tooltip")
+        stars_links=tooltip.find_elements_by_class_name("loadingLink")
+        time.sleep(4)
+        print(len(stars_links))
+
+        # list more filter, on 0=all, 1= 5 stars, 2 = 4stars, 3 = 3 stars, 4 = 2 stars, 5 = 1 star, 6 = editions all, 7 = this edition, 8 = content any, 9 = text-only
+        driver.execute_script("arguments[0].click();", stars_links[more_position]) # click with javascript, as the element ccould be hidden
+
+
+
+    for x in range(0, 10):
+        time.sleep(3)
+
+        scrape_page(review_language)
+
+        try:
+            next_button=driver.find_element_by_class_name('next_page')
+            next_button.click()
+        except:
+            print('no next button')
+            break
+
+
+
+
+
+edition_url='https://www.goodreads.com/book/show/40718205-the-dinner'
+
+
+languages=['az','id','ca','da','de','et', 'en', 'es', 'fr', 'it','lv','lt', 'hu', 'nl', 'no', 'pl', 'pt', 'ru', 'ro', 'sk','sl','fi', \
+    'sv', 'vi', 'tr', 'hr', 'is','cs', 'el', 'bg', 'mk', 'uk','he', 'ar', 'fa', 'th', 'ka']
+
+more_position=''
+
+# looping and scraping the languages
+driver = webdriver.Firefox(executable_path=r'geckodriver\geckodriver.exe')
+driver.get(edition_url)
+
+for review_language in languages:
+    print('####################################')
+    print('language = ' + review_language)
+
+    scrape_loop(driver, review_language, more_position)
+
+
+driver.close()
+
+
+
+
+# scraping reviews based on rating
+for more_position in range(3, 6):
+    #needs fresh driver for new round
+    print('#####################################################')
+    print(more_position)
+    
+    driver = webdriver.Firefox(executable_path=r'geckodriver\geckodriver.exe')
+    driver.get(edition_url)
+
+    #more_position=5 # 1=5 stars, 2 =4 stars, 3=3 stars, 4 =2 stars, 5 =1 star
+    review_language='' # must be empty, select on rating works only with all langugaes
+
+    scrape_loop(driver,review_language, more_position)
+    driver.close()
+
