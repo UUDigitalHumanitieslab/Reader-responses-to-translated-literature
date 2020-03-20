998194676f62bd72d74bce6068b4492e7453545f
unknown <robert@correcthosting.nl>
2020-03-20 15:08:55 +0100

    some improvements in button pushes

--- goodreads_scraper.py
+++ goodreads_scraper.py
@@ -13,10 +13,10 @@ from os import path
 ###########################################################################################################
 # This script scrapes all the languages in the Goodreads selectbox, and will scrape the 5 - 1 star ratings. Also it will scrape 'this edition'.
 # The star ratings are scraped for 'this edition'. But this seems not to work really well, as a lot of ratings are showing the text 'review of another edition'
-# so the only reviews that are certaint for this edition are the ones marked with scrape_type: 'edition'
-# Data are stored in a csv file. Dat are appended if the review id is not yet in the csv
-# for each review an xml file is made and stored in the folder as specified in 'scraped_folder' variable.
-# for every edition a csv file is created, as it can hold thousands of reviews. A general CSV stores al the review id's 
+# so the only reviews that are certain applicable for this edition are the ones marked with scrape_type: 'edition'
+# Data are stored in a csv file. Data are appended if the review id is not yet in the general csv of scraped reviews.
+# for each review an xml file is made and stored in the folder for the edition.
+# for every edition a new csv file is created, as it can hold thousands of reviews. A general CSV stores al the review id's 
 # and is used to check if a review already was stored, to prevent double reviews. 
 ###########################################################################################################
 
@@ -41,14 +41,16 @@ def read_editions_csv(filename):
 
 
 def make_xml_file(filename, title, review_date, review_language, review_id, author, rating, text, edition, edition_language, scrape_type):
-    scraped_folder=filename
+    scraped_folder='XML/' + filename
 
     if not path.exists(scraped_folder): 
-        try:
-            os.mkdir(scraped_folder)
-            print("Directory " , scraped_folder ,  " Created ") 
-        except FileExistsError:
-            print("Directory " , scraped_folder,  " already exists")
+        os.mkdir(scraped_folder)
+        
+        # try:
+        #     os.mkdir(scraped_folder)
+        #     print("Directory " , scraped_folder ,  " Created ") 
+        # except FileExistsError:
+        #     print("Directory " , scraped_folder,  " already exists")
 
     xml="<scrape>"
     xml+="<title>" + title +"</title>"
@@ -75,10 +77,15 @@ def make_xml_file(filename, title, review_date, review_language, review_id, auth
 
 def make_csv_file(title, review_date, review_language, review_id, author, rating, text, filename, edition_language, scrape_type):
     
+    if not path.exists('CSV/' + filename + '_scrapes_goodreads.csv'):
+        write_header=True
+    else:
+        write_header=False
+
     with open('CSV/' + filename + '_scrapes_goodreads.csv', 'a', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['review_id', 'title', 'review_date', 'review_language', 'edition_language','scrape_type', 'author', 'rating', 'text']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        if not path.exists(filename + '_scrapes_goodreads.csv'): #werkt niet
+        if write_header:
             writer.writeheader()
             print('csv created with header')
         writer.writerow({'review_id': review_id,'title':title, 'review_date':review_date, \
@@ -166,7 +173,7 @@ def scrape_page(driver, review_language, filename, title, edition, edition_langu
                 print('Review already in csv, id = ' + review_id)
                 break
 
-        if review_exists == False:
+        if review_exists == False and text != 'could not scrape': # in some editions there are many ratings without text
             make_csv_file(title, review_date, review_language, review_id, author, rating, text, filename, edition_language, scrape_type)
             make_xml_file(filename, title, review_date, review_language, review_id, author, rating, text, edition, edition_language, scrape_type)
             print('Created new line in csv, created xml file')
@@ -190,19 +197,21 @@ def scrape_loop(driver, review_language, more_position, edition, edition_languag
         filters = driver.find_element_by_css_selector("a[id^='span_class_gr-hyperlink_more_filters_']")
         hover = ActionChains(driver).move_to_element(filters)
         hover.perform()
-
+        time.sleep(2)
         tooltip=driver.find_element_by_class_name("tooltip")
         stars_links=tooltip.find_elements_by_class_name("loadingLink")
-        time.sleep(3)
-        print('must be 10, otherwise element nog found: ', len(stars_links))
+        time.sleep(2)
+        print('must be 10, otherwise element not found: ', len(stars_links))
 
         # first click position 7; this edition,
         driver.execute_script("arguments[0].click();", stars_links[7]) # click with javascript, as the element ccould be hidden
+        time.sleep(1)
+        # driver.execute_script("arguments[0].click();", stars_links[9]) # click text only. But this is not working, as it now shows reviews from other editions as well
+        # time.sleep(1)
 
-        time.sleep(2)
-
-        # list more filter, on 0=all, 1= 5 stars, 2 = 4stars, 3 = 3 stars, 4 = 2 stars, 5 = 1 star, 6 = editions all, 7 = this edition, 8 = content any, 9 = text-only
-        driver.execute_script("arguments[0].click();", stars_links[more_position]) # click with javascript, as the element ccould be hidden
+        if more_position < 7:
+            # list more filter, on 0=all, 1= 5 stars, 2 = 4stars, 3 = 3 stars, 4 = 2 stars, 5 = 1 star, 6 = editions all, 7 = this edition, 8 = content any, 9 = text-only
+            driver.execute_script("arguments[0].click();", stars_links[more_position]) # click with javascript, as the element ccould be hidden
 
 
     for x in range(0, 10):
@@ -231,7 +240,7 @@ def edition_scrape(edition_url, edition, edition_language):
     driver.close()
 
 
-def language_scrape(driver, edition_url, edition, edition_language):
+def language_scrape(edition_url, edition, edition_language):
     scrape_type='language'
     languages=['az','id','ca','da','de','et', 'en', 'es', 'fr', 'it','lv','lt', 'hu', 'nl', 'no', 'pl', 'pt', 'ru', 'ro', 'sk','sl','fi', \
     'sv', 'vi', 'tr', 'hr', 'is','cs', 'el', 'bg', 'mk', 'uk','he', 'ar', 'fa', 'th', 'ka']
@@ -250,7 +259,7 @@ def stars_scrape(edition_url, edition, edition_language):
 
     # more_position: 1 =5 stars, 2 =4 stars, 3=3 stars, 4 =2 stars, 5 =1 star
     # a lot of errors here: element not found, caused by slow internet connection? Seems random problem
-    for more_position in range(4, 6):
+    for more_position in range(1, 6):
         #needs fresh driver for new round. 
         print('#####################################################')
         print(more_position)
@@ -267,12 +276,11 @@ def stars_scrape(edition_url, edition, edition_language):
 #choose edition to scrape
 title='The Dinner' #for in the csv and XML files
 
-# change this to get a new edition from the list
-edition_from_list=3
+# change this to get a new edition from the list with editions
+edition_from_list=13
 
-# driver = webdriver.Firefox(executable_path=r'geckodriver\geckodriver.exe')
 editions=read_editions_csv('the_Dinner_editions_goodreads.csv')
-edition_languages=[ 'English', 'Dutch', 'Spanish', 'French', 'German' ]
+edition_languages=['English', 'Dutch', 'Spanish', 'French', 'German']
 editions_req_languages=[]
 
 for edition in editions:
@@ -291,9 +299,9 @@ filename=title + "_" + edition # naming the csv file
 
 
 # do functions:
-edition_scrape(edition_url, edition, edition_language)
+edition_scrape(edition_url, edition, edition_language) # This one results in fresh reviews every time
 
-#language_scrape(edition_url, edition, edition_language)
+# language_scrape(edition_url, edition, edition_language) # this is fairly independent of edition, as it overlaps all editions. Run only one time seems sufficient
 
-# stars_scrape( edition_url, edition, edition_language)
+# stars_scrape( edition_url, edition, edition_language) # about the same, most of the ratings are taken from outher editions, although 'this edition' was selected.
 
