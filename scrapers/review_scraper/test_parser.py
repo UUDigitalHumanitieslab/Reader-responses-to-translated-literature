0d9583900c8891a8a6911f01ec780f245fd34388
Alex Hebing <a.h.j.hebing@uu.nl>
2020-04-16 03:48:20 +0200

    Add a test with a long review text (#11)

--- scrapers/review_scraper/test_parser.py
+++ scrapers/review_scraper/test_parser.py
@@ -93,11 +93,11 @@ def test_get_reviews_by_count():
         assert len(p.get_reviews()) == page['review_count']
         
 
-def test_get_reviews_parsing():
+def test_get_reviews_parsing_short():
     '''
-    Test 'get_reviews' parsing capacities
+    Test 'get_reviews' parsing capacities with a short review
     '''
-    html = get_test_partial('review.html')
+    html = get_test_partial('review_short.html')
     fake_edition.url = '/parsed/for/edition_id'
     fake_edition.language = 'Klingon'
     p = ReviewPageParser(html, fake_edition)
@@ -113,6 +113,29 @@ def test_get_reviews_parsing():
     assert r.rating == 'it was amazing'
     assert r.text == 'Met veel plezier gelezen. Vlot en boeiend geschreven en af en toe een humoristische noot. Ga vast en zeker meer van hem lezen.'
 
+def test_get_reviews_parsing_long():
+    '''
+    Test 'get_reviews' parsing capacities with a long review.
+    This is interesting because long review texts are in the 'reviewText' element twice:
+    first a snippet and then the whole text.
+    '''
+    expected_text = '2/22/20. I don’t think my progress update saved on my most recent reading of this book, so since I’ve never written a review of it, now is as good a time as any. I’ve read this book so many times, and it never gets old. Read the original quite a few times, have listened to the audiobook (FANTASTIC), and now read the new illustrated version to my 6 year old while introducing him to the books for the first time. This version is beautiful, (albeit heavy) and I’m so glad we purchased it. Sharing these books with my kiddo and introducing Harry Potter for the first time is a moment I’ve waited for since he was born, and it didn’t disappoint. So much commentary from him throughout. Gasps, eyes hidden in suspense, “WHAT?!?!”, “Noo!!!”, “GRYFFINDOR!!!”, and many chuckles. We finished it last night, and he’s officially hooked. Even if I don’t always feel like I’m doing a great job as a parent, well...at least I’m raising him to love Harry Potter.'
+    
+    html = get_test_partial('review_long.html')
+    fake_edition.url = '/parsed/for/edition_id'
+    fake_edition.language = 'Klingon'
+    p = ReviewPageParser(html, fake_edition)
+    reviews = p.get_reviews()
+    assert len(reviews) == 1
+    r = reviews[0]
+    assert r.edition_id == 'edition_id'
+    assert r.edition_language == 'Klingon'
+    assert r.id == 'review_111297896'
+    assert r.url == 'https://www.goodreads.com/review/show/111297896'
+    assert r.date == 'Jul 12, 2010'
+    assert r.author == 'Erin'
+    assert r.rating == 'it was amazing'
+    assert r.text == expected_text
 
 
 def test_get_reviews_field_content():
